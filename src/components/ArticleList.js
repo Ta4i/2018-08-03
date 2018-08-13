7ce72d1047a287fd6662e009100badbe83a978df
y.golovko <yuriy.golovko@stoloto.ru>
2018-08-13 18:23:33 +0300

    ht1 p.2 added

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,27 +1,44 @@
 import * as React from "react";
 import Select from "react-select";
 import { Article } from "./Article";
-/*import DatePicker from 'react-date-picker';*/
+import DatePicker from "react-date-picker";
 
 export default class ArticleList extends React.Component {
   state = {
     openArticleId: null,
     inputValue: "",
-    date: new Date()
+    date: null
+  };
+
+  dateOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long"
   };
 
   render() {
     const { articles } = this.props;
     return (
       <div>
-        UserName:{" "}
-        <input value={this.state.inputValue} onChange={this.onChange} />
-        {/*<DatePicker />*/}
+        Выберите даты:
+        <DatePicker
+          value={null}
+          onChange={this.onChangeCalendar}
+          selectRange={true}
+        />
+        <br />
+        Диапазон дат:{" "}
+        <input
+          value={this.state.inputValue}
+          onChange={this.onChange}
+          style={{ width: "500px" }}
+        />
         <Select
           /*value={{
-            value: articles[0].id,
-            label: articles[0].title
-          }}*/
+                      value: articles[0].id,
+                      label: articles[0].title
+                    }}*/
           options={articles.map(article => ({
             value: article.id,
             label: article.title
@@ -43,9 +60,24 @@ export default class ArticleList extends React.Component {
   }
 
   onChange = event => {
-    console.log(event.target.value);
+    console.log("onChange", event.target.value);
     this.setState({
       inputValue: event.target.value
+    });
+  };
+
+  onChangeCalendar = date => {
+    //console.log('onChangeCalendar', date);
+    this.setState({
+      date: date && date.length ? date : null,
+      inputValue:
+        date && date.length
+          ? [
+              date[0].toLocaleString("ru", this.dateOptions),
+              " - ",
+              date[1].toLocaleString("ru", this.dateOptions)
+            ].join(" ")
+          : ""
     });
   };
 
